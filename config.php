800c898e113caf103e6d591652bd79571d13e643
Raiane Dev <89032013+Raiane-Dev@users.noreply.github.com>
2021-11-21 10:28:51 -0300

    Update

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-    define('INCLUDE_PATH','http://192.168.0.110/Curso/Projeto/Delivery/');
+    define('INCLUDE_PATH','http://localhost/Delivery/');
 
     define('HOST','localhost');
     define('DATABASE','sistema');
